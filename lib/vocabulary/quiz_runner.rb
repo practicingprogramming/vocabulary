67627d0c8419db1cefea77b42494c40a7b0a323c
practicingprogramming <practicingprogramming@gmail.com>
2016-01-17 12:44:15 -0800

    Colorize output

--- lib/vocabulary/quiz_runner.rb
+++ lib/vocabulary/quiz_runner.rb
@@ -1,6 +1,11 @@
+require 'colorize'
+
 module Vocabulary
   # Class responsible for reading from and writing to console.
   class QuizRunner
+    CORRECT = 'Correct!'.colorize(:green).underline
+    INCORRECT = 'Incorrect!'.colorize(:red).underline
+
     def initialize(quiz)
       @quiz = quiz
     end
@@ -11,7 +16,7 @@ module Vocabulary
         puts "Question: #{question.definition}"
         answer = gets.chomp
         result = @quiz.process_answer(question, answer)
-        puts "Result: #{result}"
+        puts result ? CORRECT : INCORRECT
       end
     end
   end
