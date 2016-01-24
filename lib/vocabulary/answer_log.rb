e1f5a233dd0f433db88362f14402ff9878a4685f
practicingprogramming <practicingprogramming@gmail.com>
2016-01-23 22:46:41 -0800

    AnswerLog.entries

--- lib/vocabulary/answer_log.rb
+++ lib/vocabulary/answer_log.rb
@@ -1,12 +1,35 @@
 module Vocabulary
   # Class responsible for tracking and persisting questions and answers.
   class AnswerLog
+    attr_reader :entries
+
     def initialize(file)
       @file = file
+      @entries = []
+      # TODO: read by line (more efficient but more difficult to tesy)
+      file.read.split("\n").each do |line|
+        next if line.empty?
+        tokens = line.split(';')
+        entry = {
+          word: tokens[0],
+          answer: tokens[1],
+          result: tokens[2] == 'true',
+          timestamp: tokens[3].to_i
+        }
+        @entries.push(entry)
+      end
     end
 
     def log(question, answer, correct)
-      @file.puts("#{question.word};#{answer};#{correct};#{Time.now.to_i}")
+      timestamp = Time.now.to_i
+      @file.puts("#{question.word};#{answer};#{correct};#{timestamp}")
+      entry = {
+        word: question.word,
+        answer: answer,
+        result: correct,
+        timestamp: timestamp
+      }
+      @entries.push(entry)
     end
   end
 end
--- spec/answer_log_spec.rb
+++ spec/answer_log_spec.rb
@@ -1,12 +1,10 @@
 RSpec.describe Vocabulary::AnswerLog do
-  before do
-    @file = double(File)
-    @answer_log = Vocabulary::AnswerLog.new(@file)
-  end
-
   describe 'log' do
     before do
+      @file = double(File)
       allow(@file).to receive(:puts).with('q1;a1;false;123')
+      allow(@file).to receive(:read) { '' }
+      @answer_log = Vocabulary::AnswerLog.new(@file)
       entry = Vocabulary::DictionaryEntry.new(
         word: 'q1',
         definition: 'd1'
@@ -17,6 +15,46 @@ RSpec.describe Vocabulary::AnswerLog do
 
     it 'logs quesion and answer' do
       expect(@file).to have_received(:puts).with('q1;a1;false;123')
+    end
+  end
+
+  describe 'entries' do
+    before do
+      @file = double(File)
+      allow(@file).to receive(:read) do
+        "q1;a1;false;123\nq2;a2;true;456"
+      end
+      @answer_log = Vocabulary::AnswerLog.new(@file)
+    end
+
+    it 'returns all asked questions, answers, results and timestamps' do
+      entries = @answer_log.entries
+      expect(entries.size).to eq(2)
+      expect(entries[0][:word]).to eq('q1')
+      expect(entries[0][:answer]).to eq('a1')
+      expect(entries[0][:result]).to eq(false)
+      expect(entries[0][:timestamp]).to eq(123)
+      expect(entries[1][:word]).to eq('q2')
+      expect(entries[1][:answer]).to eq('a2')
+      expect(entries[1][:result]).to eq(true)
+      expect(entries[1][:timestamp]).to eq(456)
+    end
+
+    it 'updates entries after logging' do
+      allow(@file).to receive(:puts).with('q3;a3;false;789')
+      entry = Vocabulary::DictionaryEntry.new(
+        word: 'q3',
+        definition: 'd3'
+      )
+      allow(Time).to receive(:now).and_return(Time.at(789))
+      @answer_log.log(entry, 'a3', false)
+
+      entries = @answer_log.entries
+      expect(entries.size).to eq(3)
+      expect(entries[2][:word]).to eq('q3')
+      expect(entries[2][:answer]).to eq('a3')
+      expect(entries[2][:result]).to eq(false)
+      expect(entries[2][:timestamp]).to eq(789)
     end
   end
 end
